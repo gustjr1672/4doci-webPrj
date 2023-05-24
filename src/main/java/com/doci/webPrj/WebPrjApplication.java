7d027640beadd4e6adac3fb062692e9b72d66a1f
HyeonSeok <gustjr2332@naver.com>
2023-05-24 16:46:06 +0900

    Initial project setting

---
+++ src/main/java/com/doci/webPrj/WebPrjApplication.java
@@ -0,0 +1,13 @@
+package com.doci.webPrj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebPrjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebPrjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/doci/webPrj/WebPrjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doci.webPrj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebPrjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
