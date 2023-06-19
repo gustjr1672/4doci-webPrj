bedb5dc6e4f0e5fc3014be2873b18e1655744d22
HyeonSeok <gustjr2332@naver.com>
2023-06-19 11:04:11 +0900

    feat: Add category register #2

--- src/main/java/com/doci/webPrj/admin/controller/CategoryController.java
+++ src/main/java/com/doci/webPrj/admin/controller/CategoryController.java
@@ -1,8 +1,33 @@
 package com.doci.webPrj.admin.controller;
 
+import com.doci.webPrj.admin.entity.Category;
+import com.doci.webPrj.admin.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("admin/category")
 public class CategoryController {
 
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping("manage")
+    public String manage() {
+        return "/admin/category/manage";
+    }
+
+    @GetMapping("register")
+    public String register(){
+        return "/admin/category/register";
+    }
+
+    @PostMapping("register")
+    public String register(Category category){
+        categoryService.save(category);
+        return "redirect:/admin/category/manage";
+    }
 }
