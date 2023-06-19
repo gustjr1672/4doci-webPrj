a85d3d2f04085409a58b485484275af7e28181fe
HyeonSeok <gustjr2332@naver.com>
2023-06-19 12:06:21 +0900

    feat: Add edit unit #2

--- src/main/java/com/doci/webPrj/admin/entity/Unit.java
+++ src/main/java/com/doci/webPrj/admin/entity/Unit.java
@@ -1,2 +1,11 @@
-package com.doci.webPrj.admin.entity;public class Unit {
+package com.doci.webPrj.admin.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Unit {
+    int id;
+    String name;
 }
--- src/main/resources/static/js/modify.js
+++ src/main/resources/static/js/modify.js
@@ -11,3 +11,16 @@ function handleOnChangeCategory(category) {
     newCategory.value = name;
     categoryId.value = id;
 }
+
+const newUnit = document.getElementById("new-unit");
+const unitId = document.getElementById("unit-id");
+function handleOnChangeUnit(unit) {
+    document.getElementById("new-unit").style.display = "block";
+
+    let name = unit.options[unit.selectedIndex].text;
+    let id = unit.options[unit.selectedIndex].value;
+
+    if (unit.selectedIndex === 0) name ="";  //선택을 클릭했을 때 빈문자열 나오도록함
+    newUnit.value = name;
+    unitId.value = id;
+}
