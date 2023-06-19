0ff24d759ff803381cc5f9165ef29eb29fa651f7
HyeonSeok <gustjr2332@naver.com>
2023-06-19 13:02:02 +0900

    feat: Add SpringSecurity config ,custom login view #1

---
+++ src/main/java/com/doci/webPrj/config/SpringSecurityConfig.java
@@ -0,0 +1,32 @@
+package com.doci.webPrj.config;
+
+import jakarta.servlet.DispatcherType;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.csrf(csrf -> csrf.disable()).cors(cors -> cors.disable())
+                .authorizeHttpRequests(request -> request
+                        .dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
+                        .requestMatchers("/image/**", "/css/**", "/member/join", "/js/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(login -> login
+                        .loginPage("/member/login")
+                        .loginProcessingUrl("/login")
+                        .usernameParameter("userId")
+                        .passwordParameter("pwd")
+                        .permitAll()
+                )
+                .logout(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
