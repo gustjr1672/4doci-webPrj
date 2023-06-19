b9c119d4b0c3219afa65bb1af9659df0dcdcd92f
HyeonSeok <gustjr2332@naver.com>
2023-06-19 10:11:21 +0900

    feat: Add main view #2

--- src/main/java/com/doci/webPrj/admin/controller/MainController.java
+++ src/main/java/com/doci/webPrj/admin/controller/MainController.java
@@ -1,7 +1,15 @@
 package com.doci.webPrj.admin.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/admin")
 public class MainController {
+
+    @GetMapping("/main")
+    public String main(){
+        return "/admin/main";
+    }
 }
