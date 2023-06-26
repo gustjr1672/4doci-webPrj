6cb70e68e781a59a33135301e0047b2c415bae78
HyeonSeok <gustjr2332@naver.com>
2023-06-26 20:18:35 +0900

    feat: Add my AllChallenges in main view #31

--- src/main/java/com/doci/webPrj/user/controller/MainController.java
+++ src/main/java/com/doci/webPrj/user/controller/MainController.java
@@ -2,6 +2,10 @@ package com.doci.webPrj.user.controller;
 
 import java.util.List;
 
+import com.doci.webPrj.admin.entity.Unit;
+import com.doci.webPrj.admin.service.UnitService;
+import com.doci.webPrj.user.entity.AllChallenges;
+import com.doci.webPrj.user.service.AllChallengesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
@@ -9,7 +13,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.doci.webPrj.config.MyUserDetails;
-import com.doci.webPrj.user.entity.Member;
 import com.doci.webPrj.user.service.NotificationService;
 
 @Controller("userMainController")
@@ -17,10 +20,19 @@ public class MainController {
 
     @Autowired
     NotificationService notificationService;
+    @Autowired
+    AllChallengesService allChallengesService;
+    @Autowired
+    UnitService unitService;
 
     @GetMapping("main")
     public String main(Model model, @AuthenticationPrincipal MyUserDetails user) {
         boolean isNoti = notificationService.getNotiStatus(user.getId());
+        List<AllChallenges> allChallengesList = allChallengesService.findAll(user.getId());
+        List<Unit> unitList = unitService.findAll();
+
+        model.addAttribute("allChallengesList", allChallengesList);
+        model.addAttribute("unitList", unitList);
         model.addAttribute("user", user);
         model.addAttribute("isNoti", isNoti);
         return "user/main";
