5370bcf822718cbedd4afcec91cb3a87e0e2bd0e
HyeonSeok <gustjr2332@naver.com>
2023-06-27 12:26:26 +0900

    feat: Add plus challenge record achievement quantity #31

---
+++ src/main/java/com/doci/webPrj/user/service/PerformanceRecordsService.java
@@ -0,0 +1,6 @@
+package com.doci.webPrj.user.service;
+
+public interface PerformanceRecordsService {
+    void updateAchvQuantity(String challengeId);
+    int getAchvQuantity(String challengeId);
+}
---
+++ src/main/java/com/doci/webPrj/user/service/PerformanceRecordsServiceImp.java
@@ -0,0 +1,76 @@
+package com.doci.webPrj.user.service;
+
+import com.doci.webPrj.user.repository.PerformanceRecordsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class PerformanceRecordsServiceImp implements PerformanceRecordsService {
+
+    private final String FREECHALLENGE = "FC";
+    private final String CHOICE = "CH";
+    private final String GROUPSTART = "GS";
+    private final String UNDERBAR = "_";
+
+
+
+    @Autowired
+    PerformanceRecordsRepository recordsRepository;
+
+
+    @Override
+    public void updateAchvQuantity(String challengeTypeAndId) {
+        List<Object> typeAndId = SeparateTypeAndId(challengeTypeAndId);
+        String type = (String) typeAndId.get(0);
+        int id = (int) typeAndId.get(1);
+
+        switch (type){
+            case FREECHALLENGE:
+                recordsRepository.updateAchvQuantity(id,null,null);
+                break;
+            case CHOICE:
+                recordsRepository.updateAchvQuantity(null, id,null);
+                break;
+            case GROUPSTART:
+                recordsRepository.updateAchvQuantity(null, null, id);
+                break;
+        }
+    }
+
+    @Override
+    public int getAchvQuantity(String challengeTypeAndId) {
+        List<Object> typeAndId = SeparateTypeAndId(challengeTypeAndId);
+        String type = (String) typeAndId.get(0);
+        int id = (int) typeAndId.get(1);
+
+        int achvQuantity = 0;
+        switch (type){
+            case FREECHALLENGE:
+                achvQuantity = recordsRepository.getAchvQuantity(id, null, null);
+                break;
+            case CHOICE:
+                achvQuantity = recordsRepository.getAchvQuantity(null, id,null);
+                break;
+            case GROUPSTART:
+                achvQuantity = recordsRepository.getAchvQuantity(null, null, id);
+                break;
+        }
+        return achvQuantity;
+    }
+
+    private List<Object> SeparateTypeAndId(String challengeId){
+        List<Object> result = new ArrayList<>();
+
+        int index = challengeId.indexOf(UNDERBAR);
+        String challengeType = challengeId.substring(0, index);
+        int id = Integer.parseInt(challengeId.substring(index + 1));
+
+        result.add(challengeType);
+        result.add(id);
+
+        return result;
+    }
+}
