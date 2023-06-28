3176c52853e88067d8c85c8877e67bcdbfd8cd4a
wognr <wognr123@gmail.com>
2023-06-28 11:12:48 +0900

    feat: Add feed detail #32

--- src/main/java/com/doci/webPrj/user/service/FeedService.java
+++ src/main/java/com/doci/webPrj/user/service/FeedService.java
@@ -3,10 +3,17 @@ package com.doci.webPrj.user.service;
 import java.util.List;
 
 import com.doci.webPrj.user.entity.Feed;
+import com.doci.webPrj.user.entity.FeedDetail;
 import com.doci.webPrj.user.entity.Member;
 
 public interface FeedService {
 
     List<Feed> getFeedList(List<Member> friendList, int userId);
+
+    List<FeedDetail> getFreeFeedList(int fcId);
+
+    List<FeedDetail> getRandomFeedList(int chId);
+
+    List<FeedDetail> getGroupFeedList(int gsId);
 
 }
