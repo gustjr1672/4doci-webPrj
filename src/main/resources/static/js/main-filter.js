e1747708be751216d54749c0b98f9c168bbdc416
HyeonSeok <gustjr2332@naver.com>
2023-06-29 19:49:00 +0900

    feat: Add challenge before start filter in main

---
+++ src/main/resources/static/js/main-filter.js
@@ -0,0 +1,90 @@
+const currentDate = new Date();
+
+function applyFilter() {
+    let selectedValue = filter.value;
+
+    myChallengeList(selectedValue);
+}
+
+
+function myChallengeList(selectedValue) {
+    let chalListSection = document.querySelector(".chal-list");
+    let chalList = chalListSection.querySelector(".chal-section");
+    let userId = chalList.dataset.userId;
+
+
+    if (selectedValue === 'on-going') {
+        fetch(`/api/all-challenges/${userId}`,)
+            .then(response => response.json())
+            .then(list => {
+                chalList.innerHTML = " ";
+
+                for (let challenge of list) {
+                    let chalStartDate = new Date(challenge.startDate);
+
+                    if (currentDate >= chalStartDate) {
+
+                        let chalTemplate =
+                            `<div class="btn-wrap" >
+                            <button class="chal-btn">
+                            <span class="chal-title" >${challenge.name}</span>
+                            <div class="prog-wrap">
+                              <div>
+                                <span class="achv-Quantity" > ${challenge.achvQuantity}</span>
+                                <span>/</span>
+                                <span >${challenge.goalQuantity} ${challenge.unitName}</span>
+                                <span hidden="hidden"class="goal_quantity">${challenge.goalQuantity}</span>
+                              </div>
+                              <div class="prog-bar">
+                                <div class="prog"
+                                     data-achv-quantity="${challenge.achvQuantity}"
+                                     data-goal-quantity="${challenge.goalQuantity}">
+                                </div>
+                              </div>
+                            </div>
+                          </button>
+                          <button class="plus-btn"
+                                  data-goal-quantity="${challenge.goalQuantity}"
+                                  data-challenge-id="${challenge.uniqueId}">+</button>
+                        <div>`
+
+                        chalList.insertAdjacentHTML("beforeend", chalTemplate);
+                    }
+                    progBar();
+                }
+            });
+    } else if (selectedValue === 'waiting') {
+        fetch(`/api/all-challenges/${userId}`,)
+            .then(response => response.json())
+            .then(list => {
+                chalList.innerHTML = " ";
+                for (let challenge of list) {
+                    let chalStartDate = new Date(challenge.startDate);
+
+                    if (currentDate <= chalStartDate) {
+                        let chalTemplate = `                
+                        <div class="btn-wrap">
+                            <button class="chal-btn">
+                                <span class="chal-title">${challenge.name}</span>
+                            </button>
+                            <div class="chal-wait">도전<br>대기중</div>
+                        </div>`
+
+                        chalList.insertAdjacentHTML("beforeend", chalTemplate);
+                    }
+                }
+            })
+    }
+}
+
+function progBar() {
+    let progressBars = document.querySelectorAll(".prog");
+
+    for (let progressBar of progressBars) {
+
+        let achvQuantity = parseInt(progressBar.dataset.achvQuantity);
+        let goalQuantity = parseInt(progressBar.dataset.goalQuantity);
+        progressBar.style.width = achvQuantity / goalQuantity * 100 + '%';
+    }
+}
+
