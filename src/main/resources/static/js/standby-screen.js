8a4cb03a7e183b5897ed38d4efcb74ee44b96118
jihye712 <jinny950712@gmail.com>
2023-07-05 12:24:55 +0900

    fix:Resolve delete button creation error in standby page #20

--- src/main/resources/static/js/standby-screen.js
+++ src/main/resources/static/js/standby-screen.js
@@ -5,132 +5,137 @@ const closeBtn = modal.querySelector(".btns button:nth-child(2)");
 const checkBoxes = modal.querySelectorAll(".content input");
 
 const friendsSection = document.querySelector(".profile-wrap");
-let profiles =  document.querySelectorAll(".profile");
+let profiles = document.querySelectorAll(".profile");
 const friendModalSection = document.querySelector(".friends-list");
 const submitBtn = document.querySelector(".submitBtn");
 const choiceModal = document.querySelector(".choice-modal");
 
-// inviteBtn.addEventListener("click", ()=>{
-//     modal.classList.remove("hidden");
-// });
-friendsSection.addEventListener("click", function(e) {
+let leaderId = document.querySelector(".leaderId").value;
+let challengeId = document.querySelector(".challengeId").value;
+let userId;
+
+friendsSection.addEventListener("click", function (e) {
     if (e.target.matches(".add-btn")) {
         modal.classList.remove("hidden");
     }
 });
-closeBtn.addEventListener("click",()=>{
+closeBtn.addEventListener("click", () => {
     modal.classList.add("hidden");
 });
 
 let selectedIds = []; // 모달에서 체크한 id 를 배열로 가져오는 함수
-friendModalSection.addEventListener("change",function(e){
-    if (e.target.matches(".checkbox")){
-        if(e.target.checked)
+friendModalSection.addEventListener("change", function (e) {
+    if (e.target.matches(".checkbox")) {
+        if (e.target.checked)
             selectedIds.push(e.target.dataset.id);
-        else{
+        else {
             let index = selectedIds.indexOf(e.target.dataset.id);
-            if (index !== -1) 
+            if (index !== -1)
                 selectedIds.splice(index, 1);
+        }
     }
-}
 })
 
-let leaderId = document.querySelector(".leaderId").value;
-let challengeId = document.querySelector(".challengeId").value;
 
-submitBtn.addEventListener("click",function(e){
-    (async()=>{
+
+submitBtn.addEventListener("click", function (e) {
+    (async () => {
         modal.classList.add("hidden");
         let formdata = new FormData();
-        formdata.append("friendList",selectedIds);
-        formdata.append("challengeId",challengeId);
+        formdata.append("friendList", selectedIds);
+        formdata.append("challengeId", challengeId);
         selectedIds = [];
-        const putResponse  = await fetch("/api/groupchallenge",{
-            method:'PUT',
+        const putResponse = await fetch("/api/groupchallenge", {
+            method: 'PUT',
             body: formdata
         })
-        if(putResponse .ok)
+        if (putResponse.ok)
             await reprintFreindSection();
     })();
 })
 
-let userId;
 
-friendsSection.addEventListener("click",function(e) {
-    console.log("실행중")
-    if(e.target.classList.contains("delete-img")){
-        console.log("여기만")
+
+friendsSection.addEventListener("click", function (e) {
+    if (e.target.classList.contains("delete-img")) {
         choiceModal.classList.remove("hidden");
         userId = e.target.dataset.id
-    }})
-
-
-choiceModal.addEventListener("click",function(e){
-    if(e.target.classList.contains("closeBtn")){
-        choiceModal.classList.add("hidden");
     }
-    else if(e.target.classList.contains("cancelBtn"))
+})
+
+
+choiceModal.addEventListener("click", function (e) {
+    if (e.target.classList.contains("closeBtn")) {
+        choiceModal.classList.add("hidden");
+        console.log("취소");
+    }
+    else if (e.target.classList.contains("cancelBtn")) {
+        console.log("삭제");
         choiceModal.classList.add("hidden");
         console.log(userId);
         console.log(challengeId);
-        (async()=>{
-            const deleteResponse  = await fetch(`/api/groupchallenge/${challengeId}/members/${userId}`,{
-                method:'DELETE',
+        (async () => {
+            const deleteResponse = await fetch(`/api/groupchallenge/${challengeId}/members/${userId}`, {
+                method: 'DELETE',
             })
-            if(deleteResponse.ok)
+            if (deleteResponse.ok)
                 await reprintFreindSection();
         })();
-    })
+    }
+})
 
- 
-function modify(){
-    let button = document.querySelector(".modifyBtn");
-    let profiles =  document.querySelectorAll(".profile");
+
+let button = document.querySelector(".modifyBtn");
+function modify() {
+    let profiles = document.querySelectorAll(".profile");
 
     if (button.textContent === "수정") {
         button.textContent = "수정취소";
-    profiles.forEach((profile) =>{
-        let deleteBtn = profile.querySelector(".delete-btn");
-        deleteBtn.classList.remove("hidden");
-        deleteBtn.classList.add("shake-animation");
+        profiles.forEach((profile) => {
+            let deleteBtn = profile.querySelector(".delete-btn");
+            deleteBtn.classList.remove("hidden");
+            deleteBtn.classList.add("shake-animation");
 
-        setTimeout(()=>{
-            deleteBtn.classList.remove("shake-animation");
-        },2000);
-    })
-}
-else{
-    button.textContent = "수정";
-        
-    profiles.forEach((profile) => {
-        let deleteBtn = profile.querySelector(".delete-btn");
-        deleteBtn.classList.add("hidden");
-    });
-}
+            setTimeout(() => {
+                deleteBtn.classList.remove("shake-animation");
+            }, 2000);
+        })
+    }
+    else {
+        button.textContent = "수정";
+
+        profiles.forEach((profile) => {
+            let deleteBtn = profile.querySelector(".delete-btn");
+            deleteBtn.classList.add("hidden");
+        });
+    }
 }
 
 
-async function reprintFreindSection(){
-    let getResponse  = await fetch(`/api/groupchallenge/${challengeId}`);
-    let data =  await getResponse .json();
+async function reprintFreindSection() {
+    let getResponse = await fetch(`/api/groupchallenge/${challengeId}`);
+    let data = await getResponse.json();
     console.log(data);
-const friendList = data.newFriendList;
-const notInviList = data.notInviList;
-console.log("친구목록",friendList);
-friendsSection.innerHTML="";
-friendsSection.insertAdjacentHTML("beforeend", 
-`<div class="add-friend">
-<button class="add-btn">+</button>
-</div>`);
+    const friendList = data.newFriendList;
+    const notInviList = data.notInviList;
+    console.log("친구목록", friendList);
+    friendsSection.innerHTML = "";
+    friendsSection.insertAdjacentHTML("beforeend",
+        `<div class="add-friend">
+            <button class="add-btn">+</button>
+        </div>`);
 
-for(const friend of friendList){
-if(friend.toMemberId !==  parseInt(leaderId)){
-let isAcceptClass = friend.isAccept === '대기중' ? 'blur' : 'hidden';
-let newFriendTemplate =`
+    for (const friend of friendList) {
+        if (friend.toMemberId !== parseInt(leaderId)) {
+            let isAcceptClass = friend.isAccept === '대기중' ? 'blur' : 'hidden';
+            let deleteBtnClass = 'delete-btn hidden';
+            if(button.textContent === "수정취소")
+                deleteBtnClass = 'delete-btn'
+            let newFriendTemplate = `
         <div class="profile"> 
             <div>
                 <div class="with-user">
-                    <button class="delete-btn" ><img class="delete-img" src="/image/startchallenge/delete.png" data-id="${friend.toMemberId}" ></button>
+                    <button class="${deleteBtnClass}" ><img class="delete-img" src="/image/startchallenge/delete.png" data-id="${friend.toMemberId}" ></button>
                     
                     <img class="img" src="${friend.profileImage}" alt="profile-img">
                     <div class="${isAcceptClass}">대기중</div>
@@ -139,25 +144,24 @@ let newFriendTemplate =`
             </div>
         </div>
 `
-friendsSection.insertAdjacentHTML("beforeend", newFriendTemplate);
-}
-}
+            friendsSection.insertAdjacentHTML("beforeend", newFriendTemplate);
+        }
+    }
 
-friendModalSection.innerHTML="";
-for(const friend of notInviList){
-modalTemplate = `
-<div class="content">
-<div class="info">
-      <img src="${friend.profileImage}" alt="프로필이미지" />
-      <div>
+    friendModalSection.innerHTML = "";
+    for (const friend of notInviList) {
+        modalTemplate = `
+        <div class="content">
+        <div class="info">
+         <img src="${friend.profileImage}" alt="프로필이미지" />
+        <div>
           <span> ${friend.nickname}</span>
           <span> ${friend.name}</span>
-      </div>
-</div>
-<input type="checkbox" class="checkbox" data-id="${friend.id}">
-`
-friendModalSection.insertAdjacentHTML("beforeend",modalTemplate)
-}
+        </div>
+        </div>
+        <input type="checkbox" class="checkbox" data-id="${friend.id}">
+        `
+        friendModalSection.insertAdjacentHTML("beforeend", modalTemplate)
+    }
 }
 
-choiceModal
