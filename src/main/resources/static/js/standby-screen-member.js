35a434f347524b459a75eecfb4f550ea7442e1d1
jihye712 <jinny950712@gmail.com>
2023-07-18 12:56:57 +0900

    style:Fix layout standby screen and add animation

--- src/main/resources/static/js/standby-screen-member.js
+++ src/main/resources/static/js/standby-screen-member.js
@@ -10,3 +10,38 @@ exitBtn.onclick = function(e){
         exitModal.classList.add("hidden");
     })
 
+    function updateTimer() {
+      const startDateSpan = document.querySelector("#startDate");
+      let startDate = startDateSpan.dataset.date;
+      const startTimeSpan = document.querySelector("#startTime");
+      let startTime = startTimeSpan.dataset.time;
+      const future = Date.parse(`${startDate} ${startTime}:00:00`);
+      const now = new Date();
+      const diff = future - now;
+    
+      const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+      const hours = Math.floor(diff / (1000 * 60 * 60));
+      const mins = Math.floor(diff / (1000 * 60));
+      const secs = Math.floor(diff / 1000);
+    
+      const d = days;
+      const h = hours - days * 24;
+      const m = mins - hours * 60;
+      const s = secs - mins * 60;
+    
+      document.getElementById("timer").innerHTML =
+        "<div>" +
+        d +
+        "<span>Days</span></div>" +
+        "<div>" +
+        h +
+        "<span>Hours</span></div>" +
+        "<div>" +
+        m +
+        "<span>Minutes</span></div>" +
+        "<div>" +
+        s +
+        "<span>Seconds</span></div>";
+    }
+    updateTimer();
+    setInterval(updateTimer, 1000);
